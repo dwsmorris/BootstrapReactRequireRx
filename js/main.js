f110297616e4c46e877cf41b187a6f5dfeeb5b2f
Darryl Morris <dwsmorris@users.noreply.github.com>
2014-12-08 11:11:02 +0000

    add polyfills for android

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 require.config({
   paths: {
-    "react": "react-with-addons",
+  	"es5-sham": "thirdParty/es5-sham",
+  	"es5-shim": "thirdParty/es5-shim",
+  	"console-polyfill": "thirdParty/console-polyfill",
+  	html5shiv: "thirdParty/html5shiv",
+  	"react": "react-with-addons",
     "JSXTransformer": "JSXTransformer"
   },
-
+  shim: {
+  	"es5-sham": {
+  		deps: ["es5-shim"]
+  	},
+  	react: {
+  		deps: ["es5-shim", "es5-sham", "console-polyfill", "html5shiv"]
+  	}
+  },
   jsx: {
     fileExtension: '.jsx'
   }
 });
 
-require(['react', 'jsx!components/Timer'], function(React, Timer) {
+require([
+	'react',
+	'jsx!components/Timer',
+	"es5-shim",
+	"es5-sham",
+	"console-polyfill",
+	"html5shiv"
+], function (React, Timer) {
   var start = new Date();
   Timer = React.createFactory(Timer);
 
